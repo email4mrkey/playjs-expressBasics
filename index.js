5009367f2dc8ff5900e2872597c1d3bc40a26242
play.js <git@gituser.com>
2021-07-01 17:39:19 +0100

    Basic routing

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-//console.log(app);
 
-app.use((req, res) => {
-  //res.send('Hello, we got your request, This is your response');
-  //res.send('<h1>Hello, this is a response</h1>');
-  res.send({color: 'red'})
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 });
 
-//app.get('/', (req, res) => {
-//  res.send('Hello World!')
-//});
+app.get('/cats', (req, res) => {
+  res.send('meeow')
+});
+
+app.get('/dogs', (req, res) => {
+  res.send('Woof')
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
